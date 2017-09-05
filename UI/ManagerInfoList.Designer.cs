11fef5eee4cd88a0be6b7af34e44c72a4736c756
RuleD <724153792@qq.com>
2017-09-05 11:03:37 +0800

    123

---
+++ UI/ManagerInfoList.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace UI
+{
+    partial class ManagerInfoList
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gvList = new System.Windows.Forms.DataGridView();
+            this.MId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_Cancel);
+            this.groupBox2.Controls.Add(this.btn_Remove);
+            this.groupBox2.Controls.Add(this.btn_Add);
+            this.groupBox2.Controls.Add(this.rb2);
+            this.groupBox2.Controls.Add(this.rb1);
+            this.groupBox2.Controls.Add(this.txtPwd);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.txtId);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox2.Location = new System.Drawing.Point(416, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(280, 301);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "添加\\修改";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(146, 166);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 3;
+            this.btn_Cancel.Text = "取消";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.Location = new System.Drawing.Point(61, 247);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(160, 23);
+            this.btn_Remove.TabIndex = 3;
+            this.btn_Remove.Text = "删除选中的店员";
+            this.btn_Remove.UseVisualStyleBackColor = true;
+            this.btn_Remove.Click += new System.EventHandler(this.btn_Remove_Click);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(36, 166);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 3;
+            this.btn_Add.Text = "添加";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Checked = true;
+            this.rb2.Location = new System.Drawing.Point(146, 130);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(47, 16);
+            this.rb2.TabIndex = 2;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "店员";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(77, 130);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(47, 16);
+            this.rb1.TabIndex = 2;
+            this.rb1.Text = "经理";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(77, 76);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(191, 21);
+            this.txtPwd.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(77, 49);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(191, 21);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(77, 24);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(191, 21);
+            this.txtId.TabIndex = 1;
+            this.txtId.Text = "添加时无编号";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(34, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "提示：双击表格项可以再此处进行修改";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Location = new System.Drawing.Point(59, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "这里只是提示，与实际密码位数无关。";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "类型：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "密码：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "用户名：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "编号：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.gvList);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(416, 301);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "列表";
+            // 
+            // gvList
+            // 
+            this.gvList.AllowUserToAddRows = false;
+            this.gvList.AllowUserToDeleteRows = false;
+            this.gvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MId,
+            this.MName,
+            this.MType});
+            this.gvList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gvList.Location = new System.Drawing.Point(3, 17);
+            this.gvList.MultiSelect = false;
+            this.gvList.Name = "gvList";
+            this.gvList.ReadOnly = true;
+            this.gvList.RowTemplate.Height = 23;
+            this.gvList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gvList.Size = new System.Drawing.Size(410, 281);
+            this.gvList.TabIndex = 0;
+            this.gvList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvList_CellDoubleClick);
+            this.gvList.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.gvList_CellFormatting);
+            // 
+            // MId
+            // 
+            this.MId.DataPropertyName = "MId";
+            this.MId.HeaderText = "编号";
+            this.MId.Name = "MId";
+            this.MId.ReadOnly = true;
+            // 
+            // MName
+            // 
+            this.MName.DataPropertyName = "MName";
+            this.MName.HeaderText = "用户名";
+            this.MName.Name = "MName";
+            this.MName.ReadOnly = true;
+            // 
+            // MType
+            // 
+            this.MType.DataPropertyName = "MType";
+            this.MType.HeaderText = "类型";
+            this.MType.Name = "MType";
+            this.MType.ReadOnly = true;
+            // 
+            // ManagerInfoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 301);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "ManagerInfoList";
+            this.Text = "店员管理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gvList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView gvList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MType;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Remove;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+    }
+}
+
