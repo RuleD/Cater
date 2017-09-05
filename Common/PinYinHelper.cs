e390dbe152af3ef462d250a95f55015ced861892
RuleD <724153792@qq.com>
2017-09-05 17:37:39 +0800

    拼音添加

---
+++ Common/PinYinHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.International.Converters.PinYinConverter;
+
+namespace Common
+{
+    public class PinYinHelper
+    {
+        public static string GetPinyin(string txt)
+        {
+            StringBuilder sb=new StringBuilder();
+            foreach (char c in txt)
+            {
+                if (ChineseChar.IsValidChar(c))
+                {
+                    ChineseChar cc=new ChineseChar(c);
+                    sb.Append(cc.Pinyins[0][0]);
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
