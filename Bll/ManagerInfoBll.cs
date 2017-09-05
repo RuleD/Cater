1b7b79036b8324bfbce61218df142f39d1d2f706
RuleD <724153792@qq.com>
2017-09-05 10:59:40 +0800

    店员管理

--- Bll/ManagerInfoBll.cs
+++ Bll/ManagerInfoBll.cs
@@ -9,9 +9,26 @@ namespace Bll
 {
     public class ManagerInfoBll
     {
+        private ManagerInfoDal miDal = new ManagerInfoDal();
         public List<ManagerInfo> GetList()
         {
-            return  new ManagerInfoDal().GetList();
+            return miDal.GetList();
+        }
+
+        public bool Add(ManagerInfo mi)
+        {
+            return miDal.Insert(mi) > 0;
+        }
+
+        public bool Remove(int id)
+        {
+            return miDal.DeleteById(id) > 0;
+        }
+
+        public bool Update(ManagerInfo mi)
+        {
+            bool b = mi.MPwd.Equals("******");
+            return miDal.Update(mi, b) > 0;
         }
     }
 }
---
+++ Common/MD5Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common
+{
+    public class MD5Helper
+    {
+        public static string GetMD5(string txt)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] bs = Encoding.UTF8.GetBytes(txt);
+            byte[] bs2=md5.ComputeHash(bs);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bs2.Length; i++)
+            {
+                sb.Append(bs2[i].ToString("x2").ToLower());
+            }
+            return sb.ToString();
+        }
+    }
+}
